b34af84c6d2255144847b1739a5d69a78af0fcd2
Kevin Vitale <kevinvitale@gmail.com>
2022-12-31 19:23:23 -0500

    [SPM] updates 'AnyCodable' to 0.6.7

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
       .library(name: "Web3Kit", targets: ["Web3Kit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.2.3"),
+        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.7"),
         .package(url: "https://github.com/KevinVitale/Wei.git", from: "1.0.0"),
     ],
     targets: [
